02c3ef39c589fa15d1b92cc86832811b2aaeade1
Dominique Dumont <dod@debian.org>
2023-12-10 18:04:29 +0100

    authordebs: remove copyright notice
    This is probvided dy dist.ini

--- lib/Dist/Zilla/App/Command/authordebs.pm
+++ lib/Dist/Zilla/App/Command/authordebs.pm
@@ -87,20 +87,6 @@ must have sudo configured properly.
 
 This command exits 1 if some required dependencies are not available as Debian packages.
 
-=head1 COPYRIGHT & LICENSE
-
-Copyright (C) 2016 Dominique Dumont <dod@debian.org>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
 =cut
 
 1;
